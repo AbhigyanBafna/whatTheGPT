eb08aea3f5a7e44664faf40914109ad610695b5e
AbhigyanBafna <101444239+AbhigyanBafna@users.noreply.github.com>
2023-02-18 15:27:31 +0530

    Updated max tokens to a reasonable amount

--- pages/api/message.js
+++ pages/api/message.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
                 prompt: req.body.Body, // completion based on this
                 temperature: 0.6, //
                 n: 1,
-                max_tokens: 50,
+                max_tokens: 300,
                 // stop: "."
             });
 
